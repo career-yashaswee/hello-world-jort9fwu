ca35b1377932fc06799ca9abe0265062d96dfad7
career-yashaswee <career.yashaswee@gmail.com>
2025-10-22 19:52:43 +0530

    fix(text)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
 return (
   <div className="App">
-    <h1>Yashasaswee Kesharwani 22BCA005</h1>
+    <h1>Yashasaswee Kesharwani ABCDE</h1>
   </div>
 );
 }
